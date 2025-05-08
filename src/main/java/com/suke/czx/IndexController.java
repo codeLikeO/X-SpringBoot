acbc0d7626ba6acf7970fe3fe1d24e44d477156a
codeLike <57949618+codeLikeO@users.noreply.github.com>
2025-05-08 09:01:06 +0800

    Update IndexController.java

--- src/main/java/com/suke/czx/IndexController.java
+++ src/main/java/com/suke/czx/IndexController.java
@@ -1,6 +1,7 @@
 
 package com.suke.czx;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 
